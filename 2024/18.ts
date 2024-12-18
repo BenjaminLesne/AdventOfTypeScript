3371221d88f37cf08bf04f1a15cbbf0a8b82e733
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-12-18 23:16:46 +0100

    18

--- 2024/18.ts
+++ 2024/18.ts
@@ -1,21 +1,9 @@
-// type Fn<T extends Array<string> = []> = <Colors extends Array<string>, A = T extends [] ? Colors : T>(colors: A, defaultColor: A[number]) => A[number]
-// type Fn = <
-//   A extends string = "",
-// 	Colors extends string[],
-// 	Default extends Colors[number] = A extends "" ? Colors[number] : A,
-// >(
-// 	colors: Colors,
-// 	defaultColor: Default,
-// ) => Colors[number];
+import { expectTypeOf } from "expect-type";
 
-type Fn = <
-	Color extends string = "",
-	Colors extends readonly string[] = Color extends "" ? readonly string[] : readonly Color[],
-	T = Colors[number],
->(
-	colors: Colors,
-	defaultColor: T,
-) => T;
+type Fn = <Color extends string, Colors extends string[] = Color[]>(
+  colors: Colors,
+  defaultColor: Colors[number]
+) => Colors[number];
 
 const createStreetLight: Fn = (colors, defaultColor) => {
 	console.log(colors);
@@ -23,20 +11,26 @@ const createStreetLight: Fn = (colors, defaultColor) => {
 };
 
 const colors = ["red" as const, "yellow" as const, "green" as const];
+type Color = (typeof colors)[number];
 
-const test = createStreetLight(colors, "blue");
+// const test = createStreetLight(colors, "blue");
 
 // @ts-expect-error (no generic parameters) blue is not a valid option
 const e0 = createStreetLight(colors, "blue");
 
 // @ts-expect-error does not accept two generic parameters, even providing a valid option
-const e1 = createStreetLight<Color, 'red'>(colors, "red");
+const e1 = createStreetLight<Color, "red">(colors, "red");
 
 // @ts-expect-error does not accept two generic parameters, and blue isn't a valid option
-const e2 = createStreetLight<Color, 'blue'>(colors, "blue");
+const e2 = createStreetLight<Color, "blue">(colors, "blue");
 
 // @ts-expect-error (with one generic parameter) blue is not a valid option
 const e3 = createStreetLight<Color>(colors, "blue");
 
+// red is a valid color, no generic parameters needed
+const t0_const = createStreetLight(colors, "red");
+expectTypeOf(t0_const).toMatchTypeOf<Color>();
 
-
+// one generic parameter is ok
+const t1_const = createStreetLight<Color>(colors, "red");
+expectTypeOf(t1_const).toMatchTypeOf<Color>();
